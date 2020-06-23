40c87815fe6fbca964a819e85aeb14b9d0bd6611
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-06-22 17:27:24 -0700

    .

--- api/models/projectModel.js
+++ api/models/projectModel.js
@@ -1,1 +1,40 @@
 const db = require('../../data/dbConfig');
+
+module.exports = {
+    add, 
+    find, 
+    findBy, 
+    findById, 
+    remove, 
+    update
+  };
+  
+  function find() {
+    return db('projects');
+  };
+  
+  
+  async function add(project) {
+    const [id] = await db('projects').insert(project);
+  
+    return findById(id);
+  };
+  
+  function findById(id) {
+    return db('projects')
+      .where({ id })
+      .first();
+  };
+  
+  function remove(id) {
+    return db('projects')
+      .where({ id })
+      .first()
+      .del();
+  };
+  
+  function update(project, id) {
+    return db('projects')
+      .where({ id })
+      .update(project);
+  };
